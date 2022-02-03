6f39159fc48090dc4b83d6c0ed0dc40e82ebf100
NeonSupernova <puma1505@outlook.com>
2022-02-02 19:55:52 -0500

    modified:   src/yt_feed/__main__.py

--- src/yt_feed/__main__.py
+++ src/yt_feed/__main__.py
@@ -1,6 +1,25 @@
 #!/usr/local/bin/python3.10
 import click
 from yt_feed import feedutils as fu
+import os
+import xdg
+import json
+
+def create_config():
+    # First run creates directory
+    if os.path.exists(f"{xdg.XDG_CONFIG_HOME}/yt-feed") != True:
+        os.mkdir(f"{xdg.XDG_CONFIG_HOME}/yt-feed")
+    # Create conf if not exists
+    if os.path.exists(f"{xdg.XDG_CONFIG_HOME}/yt-feed/yt-feed.conf") != True:
+        with open(f"{xdg.XDG_CONFIG_HOME}/yt-feed/yt-feed.conf", 'x') as f:
+            var = []
+            json.dump(var, f)
+
+    # Reading to load conf at start
+    # write to conf only with add()
+
+
+
 
 
 @click.command()
@@ -24,19 +43,25 @@ from yt_feed import feedutils as fu
     show_default=True,
     type=click.Choice(["none", "ansi", "ascii"], case_sensitive=False),
 )
-def main(sort, output_number, img, query):
-    yt_chan = [
-        "UC4qdHN4zHhd4VvNy3zNgXPA",
-        "UC6XAct-Bus4x3mYz3VC_HUw",
-        "UCwqJYTxm3FAZXeV58EdFcRQ",
-        "UCxucf8O8itcmkqu3d7yUavg",
-        "UCFqmcAvY-lLQHJcfOHJavGQ",
-        "UCeBnbqt4VRhotq2TQjkIi2A",
-        "UChd1FPXykD4pust3ljzq6hQ",
-        "UCS5Oz6CHmeoF7vSad0qqXfw",
-        "UCzTlXb7ivVzuFlugVCv3Kvg",
-    ]
-    cache = fu.Cache(yt_chan)
+@click.option(
+    "--add",
+    "-a",
+    help="Adds a link to the config"
+)
+def main(sort, output_number, img, query, add):
+    create_config()
+    conf = open(f"{xdg.XDG_CONFIG_HOME}/yt-feed/yt-feed.conf", "r")
+    if add:
+        yt_subs = json.load(conf)
+        yt_subs.append(add)
+        with open(f"{xdg.XDG_CONFIG_HOME}/yt-feed/yt-feed.conf", "w") as f:
+            json.dump(yt_subs, f)
+        return
+    try:
+        cache = fu.Cache(json.load(conf))
+    except ConfigError:
+        print(ConfigError)
+        print("Please add a channel to the list first with --add")
     cache.get()
     cache.sort(sort, output_number, query)
     for i in cache.cache_list:
